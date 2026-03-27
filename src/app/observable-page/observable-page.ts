96f0ac57512b9977076c0bb4aa3e8a59da515ddf
EmaJasekova <xjasekov@fi.muni.cz>
2026-03-27 10:48:32 +0100

    feat: add filters

--- src/app/observable-page/observable-page.ts
+++ src/app/observable-page/observable-page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, map, filter } from 'rxjs';
 
 @Component({
   selector: 'app-observable-page',
@@ -8,13 +8,17 @@ import { interval, Subscription } from 'rxjs';
   styleUrl: './observable-page.css',
 })
 export class ObservablePage {
-  value: number = 0;
+  value: string = "Value: 0";
   subscription: Subscription | null = null;
 
   startObservable() {
     if (!this.subscription) {
-      this.subscription = interval(1000).subscribe((value) => {
-        this.value = value;
+      this.subscription = interval(1000).pipe(
+        filter((value: number) => value > 2),
+        map((value: number) : number => value * 10),
+        map((value: number) : string => "Value: " + value)
+      ).subscribe({
+        next: (value: string) => this.value = value
       });
     }
   }
